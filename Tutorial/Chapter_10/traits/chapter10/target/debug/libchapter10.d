2ce259d0e17a3750d23bdfd73572d1fdabcb339b
Kieun Kim <78407737+eunice-nxn@users.noreply.github.com>
2021-09-14 16:36:41 +0900

    Delete Tutorial/Chapter_10/traits/chapter10/target directory

--- Tutorial/Chapter_10/traits/chapter10/target/debug/libchapter10.d
+++
@@ -1,1 +0,0 @@
-/Users/kieunkim/Desktop/Rust-Programming/Tutorial/Chapter_10/traits/chapter10/target/debug/libchapter10.rlib: /Users/kieunkim/Desktop/Rust-Programming/Tutorial/Chapter_10/traits/chapter10/src/lib.rs
